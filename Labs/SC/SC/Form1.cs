18b06639b98843ce8f56be5a3a79baf437da01bd
Timotin Mihai <mihaitimotin@gmail.com>
2021-09-25 15:21:21 +0300

    lab 1

---
+++ Labs/SC/SC/Form1.cs
@@ -0,0 +1,52 @@
+﻿using SC.Parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SC
+{
+     public partial class Form1 : Form
+     {      
+          ParserServices _parser = new ParserServices();
+
+          public Form1()
+          {
+               InitializeComponent();
+          }
+
+          private void Browse_Click(object sender, EventArgs e)
+          {
+               OpenFileDialog im = new OpenFileDialog();
+               im.Title = "Select Polices";
+               im.Filter = "All files(*.*)|*.*|All files(*.*)|*.*";
+               im.FilterIndex = 100;
+               im.RestoreDirectory = true;
+               if (im.ShowDialog() == DialogResult.OK)
+               {
+                    textBox1.Text = im.FileName;
+               }
+
+          }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+               var im = new FolderBrowserDialog();              
+               if (im.ShowDialog() == DialogResult.OK)
+               {
+                    textBox2.Text = im.SelectedPath;
+               }
+          }
+
+          private void button2_Click(object sender, EventArgs e)
+          {
+               if(textBox1.Text.Length > 0 && textBox2.Text.Length > 0 && textBox3.Text.Length > 0)
+               {
+                    _parser.GetFileText(textBox1.Text, textBox2.Text, textBox3.Text);
+               }
+          }
+     }
+}
---
+++ Labs/SC/SC/Parser/ParserServices.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SC.Parser
+{
+     class ParserServices
+     {
+          public void GetFileText(string filePatch, string jsonPath, string jsonName)
+          {
+               List<string> items = new List<string>();
+               List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+
+               string text = File.ReadAllText(filePatch);
+               items = GetListOfItems(text);
+               list = splitRules(items);
+               generateJson(list, jsonPath, jsonName);
+
+          }
+
+          private void generateJson(List<Dictionary<string, string>> jsonTxt, string jsonPath, string jsonName)
+          {
+               string path = jsonPath + "\\" + jsonName + ".json";
+               string json = JsonConvert.SerializeObject(jsonTxt, Formatting.Indented);
+
+               using (FileStream fs = File.Create(path))
+               {
+                    byte[] info = new UTF8Encoding(true).GetBytes(json);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+               }
+          }
+
+          private List<string> GetListOfItems(string text)
+          {
+               List<string> items = new List<string>();             
+               for (int i = 1; i < text.Split("<custom_item>").Length; i++)
+               {
+                    var item = text.Split("<custom_item>")[i].Split("</custom_item>")[0];
+                    items.Add(item);
+               }
+               return items;
+          }
+
+          private List<Dictionary<string, string>> splitRules(List<string> items)
+          {
+               List<Dictionary<string, string>> list = new List<Dictionary<string, string>>();
+               Dictionary<string, string> dictionary;
+               foreach (var item in items)
+               {
+                    dictionary = new Dictionary<string, string>();
+                    var e = item.Split("\n ");
+                    foreach(var i in e)
+                    {    
+                         if(i.Contains(':'))
+                         {
+                              var j = i.Split(":");
+                              j[0].Trim();
+                              dictionary.Add(j[0], j[1]);
+                         }
+                    }
+                    list.Add(dictionary);
+               }
+               return list;
+          }
+     }
+}
